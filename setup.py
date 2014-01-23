5d147c8e00813bc9e35460ab9d111141eaa308d9
Robert Niederreiter <office@squarewave.at>
2014-01-23 08:56:16 +0100

    housekeeping

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
-from setuptools import (
-    setup,
-    find_packages,
-)
+from setuptools import setup
+from setuptools import find_packages
 
 
 version = '0.3'
@@ -12,41 +10,42 @@ longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
 
-setup(name='bda.plone.cart',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          'Environment :: Web Environment',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-      ],
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      license='GNU General Public Licence',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['bda', 'bda.plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'simplejson',
-          'Plone',
-          'bda.plone.ajax',
-          'bda.plone.shipping',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'bda.plone.cart',
-              'mock',
-          ]
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='bda.plone.cart',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    license='GNU General Public Licence',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['bda', 'bda.plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'simplejson',
+        'Plone',
+        'bda.plone.ajax',
+        'bda.plone.shipping',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'bda.plone.cart',
+            'mock',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
